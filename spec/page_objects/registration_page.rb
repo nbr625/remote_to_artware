641a7481d70d20a9376555c441d16af7a8a0bf64
nbr625 <nbr625@gmail.com>
2015-10-24 15:41:47 -0400

    added skeleton of registration object page

---
+++ spec/page_objects/registration_page.rb
@@ -0,0 +1,21 @@
+class RegistrationPage < AbstractPage
+	
+	def fill_in_email
+	end
+
+	def fill_in_passworld
+	end
+
+	def submit
+	end
+
+	def submit_invalid_registration_form
+	end
+
+	def get_success_confirmation
+	end
+
+	def get_invalid_sign_up_alert
+	end
+
+end
